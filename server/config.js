05835a81832135d767f9c23ab96a6c48ff41c03a
JamesKennethRobinson <Jameskennthrobinson@gmail.com>
2015-07-10 14:13:18 -0700

    (feature) progress on facebook api request

--- server/config.js
+++ server/config.js
@@ -2,15 +2,47 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var Twitter = require('twitter');
 var util = require('./utils.js');
+var everyauth = require('everyauth');
+var request = require('request');
 
 
-var app = express();
+app = express();
 
-app.use(bodyParser.json());
+everyauth.helpExpress(app);
+
+//app.use(bodyParser.json());
+
+//routes
+
+request({
+	uri: 'http://graph.facebook.com/endpoint?key=value&access_token=app_id|app_secret',
+	method: 'GET',
+	body: {
+		code: param,
+		'app_id': '1588095601452484',
+		'app_secret': '87e7379e401ee2b7a1c02979f9e30e5f'
+	},
+	function(error, response, body){
+		if (error){
+			return console.error('error: ', error)
+		}
+		console.log('success');
+	};
+
+})
+
+app.get('/', function(req, res){
+	console.log('----', req.body);
+	res.send('hello world');
+});
+
+app.get('/tweet', util.getTwitterData);
+
+app.get('/facebook', function(req, res){
+	console.log('hello world');
+});
 
 
-
-app.get('/', util.getTwitterData);
 
 
 
--- server/utils.js
+++ server/utils.js
@@ -4,39 +4,39 @@ var Twitter = require('twitter');
 // !!! IMPORTANT !!! --> NEED TO HIDE TWITTER APP DATA BELOW
 ////////
 
+
+
+exports.getTwitterData = function(req, res){
+
 var client = new Twitter({
 	consumer_key: 's0E55g1fs108kSjWVycCarT64',
 	consumer_secret: 'LXYNAjWQcJ1UjpTrsPdmqWmdSAv5wTFNdYLhOVELFgFnE5OEXN',
 	access_token_key: '874702442-UH5dCPdQ2tyl6NiqbwPFhyzsFNOYbFDdzQiuC0ar',
 	access_token_secret: 'QLDf9QCxUzMxD7FkXMkTDKSmM5bB3Fe3ypvbw4Gq1GpAv'
 });
-
-exports.getTwitterData = function(req, res){
-
-
-	// client.get('favorites/list', function(error, tweets, response){
-	//   if(error) throw error;
-	//   console.log(response);  // The favorites. 
-	//   res.send(response);
-	// });
-
-	client.stream('statuses/filter', {track: 'giants'}, function(stream){
+	client.stream('statuses/filter', {track: 'Cuba'}, function(stream){
 
 		stream.on('data', function(tweet){
-			console.log(tweet)
+			//can use : geo, place, coordinates, location
+			// if (tweet.geo || tweet.place || tweet.coordinates || tweet.location){
+			// 	console.log(tweet.geo);
+			// 	console.log(tweet.place);
+			// 	console.log(tweet.coordinates);
+			// 	console.log(tweet.location);
+			// }
+			console.log(tweet.place);
 		});
 
 		stream.on('error', function(error){
 			throw error;
 		});
 	});
-
 };
 
 
-exports.testFunction = function(req, res){
-	console.log('hello');
-	res.send('hello');
+exports.getFacebookData = function(req, res){
+
 };
+
 
 //https://stream.twitter.com/1.1/statuses/filter.json?
