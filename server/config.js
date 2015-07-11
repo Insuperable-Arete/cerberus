e69d605f6778a5e9ec98514b0b2107966e5e51ba
JamesKennethRobinson <Jameskennthrobinson@gmail.com>
2015-07-11 12:13:33 -0700

    (feat) served dummy data to '/dummy' route

--- server/config.js
+++ server/config.js
@@ -1,51 +1,22 @@
+//node moduels
 var express = require('express');
 var bodyParser = require('body-parser');
 var Twitter = require('twitter');
-var util = require('./utils.js');
-var everyauth = require('everyauth');
 var request = require('request');
 
+//local modules
+var util = require('./utils.js');
 
 app = express();
 
-everyauth.helpExpress(app);
-
-//app.use(bodyParser.json());
 
 //routes
-
-request({
-	uri: 'http://graph.facebook.com/endpoint?key=value&access_token=app_id|app_secret',
-	method: 'GET',
-	body: {
-		code: param,
-		'app_id': '1588095601452484',
-		'app_secret': '87e7379e401ee2b7a1c02979f9e30e5f'
-	},
-	function(error, response, body){
-		if (error){
-			return console.error('error: ', error)
-		}
-		console.log('success');
-	};
-
-})
-
 app.get('/', function(req, res){
 	console.log('----', req.body);
 	res.send('hello world');
 });
 
-app.get('/tweet', util.getTwitterData);
-
-app.get('/facebook', function(req, res){
-	console.log('hello world');
-});
-
-
-
-
-
+app.get('/dummy', util.sendDummyData);
 
 
 module.exports = app;
