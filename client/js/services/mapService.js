364a9a5d44348917d9c85f2cc60b1d1a503fedc6
Justin Fong <justinf715@gmail.com>
2015-07-31 19:47:17 -0700

    (fix) Overview now showing correct information

--- client/js/services/mapService.js
+++ client/js/services/mapService.js
@@ -16,7 +16,6 @@ angular.module('app.mapService', [])
     };
 
     var updateBeachInfo = function() {
-      console.log('updating beach info')
       if (!currentBeach) {
         $state.go('default');
         throw new Error('Error: updateBeachInfo failed, no beach selected');
@@ -36,8 +35,9 @@ angular.module('app.mapService', [])
     var getBeachData = function() {
       return $http({
         method: 'GET',
-        url: '/fetch'
+        url: 'http://localhost:1337/fetch'
       }).then(function (resp) {
+        console.log(resp.data);
         return resp.data;
       });
     };
